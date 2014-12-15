790c4fcd81c237138fd218914815e30f686cc34c
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2014-12-15 17:17:51 +0100

    fixed css

--- js/bezel.js
+++ js/bezel.js
@@ -3,3 +3,12 @@
 *	- all functions that handle touch events are here
 *	Authors: LL + AJ
 */
+
+// touch event
+window.addEventListener('load', function(){ // on page load
+ 
+ document.body.addEventListener('touchstart', function(e){
+  alert(e.changedTouches[0].pageX) // alert pageX coordinate of touch point
+ }, false)
+ 
+}, false)
