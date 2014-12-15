0cf8f612c923778d7aa0b9241a1d51d8c6bd982f
Leonardo Lanzinger <leonardo.lanzinger@gmail.com>
2014-12-15 13:49:30 +0100

    helper file

--- js/bezel.js
+++ js/bezel.js
@@ -1,46 +1,5 @@
-/**
- * Determine the mobile operating system.
- * This function either returns 'iOS', 'Android' or 'unknown'
- *
- * @returns {String}
- */
-function getMobileOperatingSystem() {
-  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
-
-  if( userAgent.match( /iPad/i ) || userAgent.match( /iPhone/i ) || userAgent.match( /iPod/i ) )
-  {
-    return 'iOS';
-
-  }
-  else if( userAgent.match( /Android/i ) )
-  {
-
-    return 'Android';
-  }
-  else
-  {
-    return 'unknown';
-  }
-}
-
-function loadCss(index) {
-	var head  = document.getElementsByTagName('head')[0];
-    var link  = document.createElement('link');
-    link.id   = "css";
-    link.rel  = 'stylesheet';
-    link.type = 'text/css';
-    if (index == 0) {
-	    link.href = 'css/ios.css';
-    }
-    else if (index == 1) {
-    	link.href = 'css/android.css';
-    }
-    link.media = 'all';
-    head.appendChild(link);
-}
-
-if ('ontouchstart' in window) {
-   window.addEventListener('load', function() {
-      FastClick.attach(document.body);
-   }, false);
-}
+/*
+*	Bezel Lockscreen main Javascript file
+*	- all functions that handle touch events are here
+*	Authors: LL + AJ
+*/
